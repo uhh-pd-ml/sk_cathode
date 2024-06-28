c54a16bff13a5687d8385719ece12ea8b0f28666
Manuel Sommerhalder <m.r.sommerhalder@hotmail.com>
2024-06-28 13:12:34 +0200

    adding dedicated sic_curve function

--- sk_cathode/utils/evaluation_functions.py
+++ sk_cathode/utils/evaluation_functions.py
@@ -3,6 +3,62 @@ import matplotlib.gridspec as gridspec
 import numpy as np
 
 from matplotlib import colors
+from sklearn.metrics import roc_curve
+
+
+def sic_curve(y_true, y_score, pos_label=None, sample_weight=None):
+    """
+    Compute the significance improvement characteristic (SIC) curve.
+    The function is based on sklearn.metrics.roc_curve and supports
+    the same arguments.
+
+    Parameters
+    ----------
+    y_true : array-like of shape (n_samples,)
+        True binary labels. If labels are not either {-1, 1} or {0, 1}, then
+        pos_label should be explicitly given.
+
+    y_score : array-like of shape (n_samples,)
+        Target scores, can either be probability estimates of the positive
+        class, confidence values, or non-thresholded measure of decisions
+        (as returned by "decision_function" on some classifiers).
+
+    pos_label : int, float, bool or str, default=None
+        The label of the positive class.
+        When ``pos_label=None``, if `y_true` is in {-1, 1} or {0, 1},
+        ``pos_label`` is set to 1, otherwise an error will be raised.
+
+    sample_weight : array-like of shape (n_samples,), default=None
+        Sample weights.
+
+    Returns
+    -------
+    sic : array-like of shape (n_samples,)
+        Increasing SIC values: True Positive Rate / sqrt(False Positive Rate),
+        such that element i is the false positive rate of predictions with
+        score >= thresholds[i].
+
+    fpr : ndarray of shape (>2,)
+        Increasing false positive rates such that element i is the false
+        positive rate of predictions with score >= `thresholds[i]`.
+
+    tpr : ndarray of shape (>2,)
+        Increasing true positive rates such that element `i` is the true
+        positive rate of predictions with score >= `thresholds[i]`.
+
+    thresholds : ndarray of shape (n_thresholds,)
+        Decreasing thresholds on the decision function used to compute
+        fpr and tpr. `thresholds[0]` represents no instances being predicted
+        and is arbitrarily set to `np.inf`.
+    """
+
+    with np.errstate(divide='ignore', invalid='ignore'):
+        fpr, tpr, thresholds = roc_curve(y_true, y_score,
+                                         pos_label=pos_label,
+                                         sample_weight=sample_weight)
+        sic = tpr / np.sqrt(fpr)
+
+    return sic, fpr, tpr, thresholds
 
 
 def hists_2d(data, labels=None, weights=None, n_features=None, bins=30,
